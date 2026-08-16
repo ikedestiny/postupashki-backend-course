3de2f6e458ffb9a93bb107e5b38992797f098ab7
Ike Destiny <ikedestiny18@gmail.com>
2026-08-16 15:49:33 +0300

    first commit

---
+++ start/hw1/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+type fetchResult struct {
+	url     string
+	body    []byte
+	status  string
+	headers http.Header
+	err     error
+}
+
+func main() {
+
+	timeout := flag.Int("t", 15, "timeout option")
+	help := flag.Bool("h", false, "Help option")
+	flag.Parse()
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
+
+	if *help {
+		flag.Usage() // This prints the default usage text (which includes your flags)
+		return       // Exit the program
+	}
+
+	urls := flag.Args()
+
+	if len(urls) == 0 {
+		fmt.Println("No Urls was entered!!!!")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	ch := make(chan fetchResult, len(urls))
+
+	for _, url := range urls {
+		go func(url string) {
+			req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+
+			if err != nil {
+				ch <- fetchResult{url: url, err: err}
+			} else {
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					ch <- fetchResult{url: url, err: err}
+					return
+				}
+				body, err := io.ReadAll(resp.Body)
+				defer resp.Body.Close()
+
+				ch <- fetchResult{url: url, body: body, status: resp.Status, headers: resp.Header, err: err}
+			}
+
+		}(url)
+	}
+
+	errorCount := 0
+	for {
+		select {
+		case res := <-ch:
+			if res.err != nil {
+				errorCount++
+				if errorCount == len(urls) {
+					fmt.Println("All requests failed")
+					os.Exit(1)
+				}
+			} else {
+				fmt.Println(res.status) // e.g., "200 OK"
+				for key, values := range res.headers {
+					for _, value := range values {
+						fmt.Printf("%s: %s\n", key, value)
+					}
+				}
+				fmt.Println()               // Blank line between headers and body
+				fmt.Print(string(res.body)) // Print the raw body
+				os.Exit(0)
+			}
+
+		case <-ctx.Done():
+			fmt.Println("Request Timed out!!!!")
+			cancelFunc()
+			os.Exit(228)
+		}
+	}
+}
